4367886f1f5996cc8a5da1ccbcd599e55b16c2d3
kingMordred <ayoubayari47@gmail.com>
2025-05-12 14:51:01 +0100

    handle ctrl+c signal

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
+/*
+    The goal is a simple http server that can serve a .html file
+
+    Learning source: https://beej.us/guide/bgnet/html/#intro
+    getaddrinfo -> socket -> bind -> listen -> accept -> fork -> send/recv
+*/
+
 #include "http.h"
 
 int main(int argc, char *argv[])
 {
-    int status, sockfd, new_fd;
+    int status, sockfd, new_fd, yes = 1;
     struct addrinfo hints, *res;
     struct sockaddr_storage client_addr;
     socklen_t addr_size;
+    char s[INET6_ADDRSTRLEN], buf[MAX_BUFF_LEN];
+
+    if(signal(SIGINT, sig_handler) == SIG_ERR)
+    {
+        perror("cannot handle sigint");
+        exit(EXIT_FAILURE);
+    }
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
@@ -21,6 +35,13 @@ int main(int argc, char *argv[])
     if((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
     {
         perror("server socket");
+        exit(1);
+    }
+
+    //reuse a previously bound port
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+        perror("setsockopt");
         exit(1);
     }
 
@@ -50,10 +71,27 @@ int main(int argc, char *argv[])
             perror("new fd");
             exit(1);
         }
-        printf("connection accepted for now\n");
+        inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr*) &client_addr), s, sizeof(s));
+
+
+        printf("connection accepted from: %s\n", s);
+
+        //fork and read/send
+        if(!fork())
+        {
+            close(sockfd);
+
+            if(send(new_fd, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, world!", MAX_BUFF_LEN, 0) == -1)
+            {
+                perror("send");
+            }
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);
     }
 
 
-    
-    
+    return EXIT_SUCCESS;
+  
 }
